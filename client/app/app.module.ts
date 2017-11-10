e5d5741fc878b92a9f75a3b0835f0f37c5eee5f0
Wally <ojniyah@gmail.com>
2017-11-10 05:40:09 -0500

    Created a component and add it to the  application

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {AppComponent} from './app.Component';
+
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
